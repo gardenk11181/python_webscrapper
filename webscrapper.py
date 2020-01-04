09b2e262602879e8df8f00069a4d2c35c151b40f
gardenk11181 <55073876+gardenk11181@users.noreply.github.com>
2020-01-04 18:50:27 +0900

    Finish Making WebScrapping-Functional

--- webscrapper.py
+++ webscrapper.py
@@ -2,7 +2,11 @@ from indeed import get_jobs as get_indeed_jobs
 from stackoverflow import get_jobs as get_so_jobs
 from save import save_to_file
 
-indeed_jobs = get_indeed_jobs()
-so_jobs = get_so_jobs()
-jobs = indeed_jobs+so_jobs
-save_to_file(jobs)
+def save_jobs_csv():
+    indeed_jobs = get_indeed_jobs()
+    so_jobs = get_so_jobs()
+    jobs = indeed_jobs + so_jobs
+    save_to_file(jobs)
+
+save_jobs_csv()
+
